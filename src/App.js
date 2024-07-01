03dc4db6cc16f68348e2ea3482fd7fa50ec4b182
Vishal Dhanotiya <vishal_dhanotiya@Vishals-MacBook-Pro.local>
2024-07-01 11:51:29 +0530

    Improve UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "../src/App.css";
+
+const PROD_URL="https://app.tasktag.com/register/signup-with-email?invitation_token="
+const DEV_URL="https://beta.app.tasktag.com/register/signup-with-email?invitation_token=" 
 function App() {
   let invitation_token = "";
   let env_prod = "";
+  
 
   const queryParameters = new URLSearchParams(window.location.search);
+  
   invitation_token = queryParameters.get("invitation_token") ?? "";
   env_prod = queryParameters.get("env_prod") ?? "";
   if (invitation_token == null) {
@@ -12,7 +17,19 @@ function App() {
     console.log("invitation_token", invitation_token);
     console.log("env_prod", env_prod);
   } else {
-    console.log("invitation_token", invitation_token);
+    console.log("invitation_token else", invitation_token);
+    console.log("env_prod else", env_prod);
+
+  }
+
+  const appendToken=(env,token)=>{
+    if(env){
+return PROD_URL+token
+    }else{
+      return DEV_URL+token
+
+    }
+
   }
 
   return (
@@ -26,19 +43,24 @@ function App() {
             src={require("./res/appIcon.png")}
           />
 
-          <div>
-            <div className="button-app">
+          <div className="center-div" >
+          <p className="text">If you already have the task tag application. Please click on this button</p>
+
+            <div className="button">
               <a href={"com.eloveit.tasktag://register/" + invitation_token}>
                 Open TaskTag App
               </a>
             </div>
             <br></br>
     
-            <div className="button-web">
+            <p className="text">If you don't have the task tag application. Please click on this button</p>
+
+            <div className="button">
+           
               <a target="_blank"
                 href={
-                  env_prod ? "https://app.tasktag.com/register/signup-with-email?invitation_token=" :"https://beta.app.tasktag.com/register/signup-with-email?invitation_token="  +
-                  invitation_token
+                  appendToken( env_prod,
+                  invitation_token)
                 } rel="noreferrer">
                 Open TaskTag Web
               </a>
