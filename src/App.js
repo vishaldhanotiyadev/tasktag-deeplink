629307dd004545557a322ca7acf1d24ed323b5c0
Vishal Dhanotiya <vishal_dhanotiya@Vishals-MacBook-Pro.local>
2024-07-01 12:47:43 +0530

    Improve UI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "../src/App.css";
 
-const PROD_URL="https://app.tasktag.com/register/signup-with-email?invitation_token="
-const DEV_URL="https://beta.app.tasktag.com/register/signup-with-email?invitation_token=" 
-function App() {
+const PROD_URL =
+  "https://app.tasktag.com/register/signup-with-email?invitation_token=";
+const DEV_URL =
+  "https://beta.app.tasktag.com/register/signup-with-email?invitation_token=";
+
+const App = () => {
   let invitation_token = "";
   let env_prod = "";
-  
 
   const queryParameters = new URLSearchParams(window.location.search);
-  
+
   invitation_token = queryParameters.get("invitation_token") ?? "";
   env_prod = queryParameters.get("env_prod") ?? "";
   if (invitation_token == null) {
@@ -19,18 +21,16 @@ function App() {
   } else {
     console.log("invitation_token else", invitation_token);
     console.log("env_prod else", env_prod);
-
   }
 
-  const appendToken=(env,token)=>{
-    if(env){
-return PROD_URL+token
-    }else{
-      return DEV_URL+token
-
+  const appendToken = (env, token) => {
+    console.log("&&&&&&&&===" + env + "====" + token);
+    if (env) {
+      return PROD_URL + token;
+    } else {
+      return DEV_URL + token;
     }
-
-  }
+  };
 
   return (
     <html>
@@ -43,8 +43,11 @@ return PROD_URL+token
             src={require("./res/appIcon.png")}
           />
 
-          <div className="center-div" >
-          <p className="text">If you already have the task tag application. Please click on this button</p>
+          <div className="center-div">
+            <p className="text">
+              If you already have the task tag application. Please click on this
+              button
+            </p>
 
             <div className="button">
               <a href={"com.eloveit.tasktag://register/" + invitation_token}>
@@ -52,16 +55,18 @@ return PROD_URL+token
               </a>
             </div>
             <br></br>
-    
-            <p className="text">If you don't have the task tag application. Please click on this button</p>
+
+            <p className="text">
+              If you don't have the task tag application. Please click on this
+              button
+            </p>
 
             <div className="button">
-           
-              <a target="_blank"
-                href={
-                  appendToken( env_prod,
-                  invitation_token)
-                } rel="noreferrer">
+              <a
+                target="_blank"
+                href={appendToken(env_prod, invitation_token)}
+                rel="noreferrer"
+              >
                 Open TaskTag Web
               </a>
             </div>
@@ -70,6 +75,6 @@ return PROD_URL+token
       </body>
     </html>
   );
-}
+};
 
 export default App;
